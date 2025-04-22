e28765f5f9c0fce40aed859175fd6b97cba34135
Lokesh Patel <64927810+LowCASHH@users.noreply.github.com>
2025-04-22 13:29:37 +0900

    removed comments

--- CtrlBoard-H7_4310/User/bsp/can_bsp.c
+++ CtrlBoard-H7_4310/User/bsp/can_bsp.c
@@ -96,7 +96,7 @@ uint8_t canx_send_data(FDCAN_HandleTypeDef *hcan, uint16_t id, uint8_t *data, ui
   TxHeader.TxFrameType = FDCAN_DATA_FRAME;  
   if(len<=8)	
 	{
-	  TxHeader.DataLength = len<<16;     // 发送长度：8byte
+	  TxHeader.DataLength = len<<16;     
 	}
 	else  if(len==12)	
 	{
@@ -123,10 +123,10 @@ uint8_t canx_send_data(FDCAN_HandleTypeDef *hcan, uint16_t id, uint8_t *data, ui
 	 }
 											
 	TxHeader.ErrorStateIndicator =  FDCAN_ESI_ACTIVE;
-  TxHeader.BitRateSwitch = FDCAN_BRS_ON;//比特率切换关闭，
+  TxHeader.BitRateSwitch = FDCAN_BRS_ON;
   TxHeader.FDFormat =  FDCAN_FD_CAN;
   TxHeader.TxEventFifoControl =  FDCAN_NO_TX_EVENTS;  
-  TxHeader.MessageMarker = 0;//消息标记
+  TxHeader.MessageMarker = 0;
 	
    
   if(HAL_FDCAN_AddMessageToTxFifoQ(hcan, &TxHeader, data) != HAL_OK)
@@ -146,27 +146,26 @@ void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
     if(hfdcan->Instance == FDCAN1)
     {
       /* Retrieve Rx messages from RX FIFO0 */
-			memset(g_Can1RxData, 0, sizeof(g_Can1RxData));	//接收前先清空数组	
-			while(HAL_FDCAN_GetRxFifoFillLevel(&hfdcan1, FDCAN_RX_FIFO0) > 0){
-				HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &RxHeader1, g_Can1RxData);
-				EventRecord2(0x01, RxHeader1.Identifier, HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO0));
-				int motor_id = -1;  // Initialize motor_id as invalid
-				if (RxHeader1.Identifier != 0)
-				{
-						motor_id = RxHeader1.Identifier - 0x11; // Assuming motor IDs start from 0x11
-				}
-				else
-				{
-						motor_id = (g_Can1RxData[0]) & 0x0F;  // Mask to extract lower 4 bits
-				}
-				if (motor_id >= 0 && motor_id < NUM_MOTORS)
-				{
-						dm_fbdata(&arm.motors[motor_id], g_Can1RxData, RxHeader1.DataLength);
-				}
-				else
-				{
-						printf("Invalid motor ID: %d (CAN ID: 0x%X)\n\r", motor_id, RxHeader1.Identifier);
-				}
+			memset(g_Can1RxData, 0, sizeof(g_Can1RxData));
+			HAL_FDCAN_GetRxMessage(hfdcan, FDCAN_RX_FIFO0, &RxHeader1, g_Can1RxData);
+			EventRecord2(0x01, RxHeader1.Identifier, HAL_FDCAN_GetRxFifoFillLevel(hfdcan, FDCAN_RX_FIFO0));
+			printf("message identifier: %d\n", RxHeader1.Identifier-0x10);
+			int motor_id = -1;  // Initialize motor_id as invalid
+			if (RxHeader1.Identifier != 0)
+			{
+					motor_id = RxHeader1.Identifier - 0x11; // Assuming motor IDs start from 0x11
+			}
+			else
+			{
+					motor_id = (g_Can1RxData[0]) & 0x0F;  // Mask to extract lower 4 bits
+			}
+			if (motor_id >= 0 && motor_id < NUM_MOTORS)
+			{
+					dm_fbdata(&arm.motors[motor_id], g_Can1RxData, RxHeader1.DataLength);
+			}
+			else
+			{
+					printf("Invalid motor ID: %d (CAN ID: 0x%X)\n\r", motor_id, RxHeader1.Identifier);
 			}
 		}			
 	}
